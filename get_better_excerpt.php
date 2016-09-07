5295ebafafaa652c175db11f2bb41b080c04adf2
stormrockwell <storm@stormrockwell.com>
2016-09-07 16:39:39 -0400

    Update get_better_excerpt.php

--- get_better_excerpt.php
+++ get_better_excerpt.php
@@ -1,28 +1,29 @@
 <?php
-	/**
-	 * Get Better Excerpt
-	 * @param  mixed   $post  int or 
-	 * @param  integer $count max length of excerpt
-	 * @return string         the excerpt
-	 */
-	function get_better_excerpt( $post, $count = 150 ) {
 
-		if ( ! is_object( $post ) ) {
-			$post = get_post( $post );
+/**
+ * Get Better Excerpt
+ * @param  mixed   $post  int or 
+ * @param  integer $count max length of excerpt
+ * @return string         the excerpt
+ */
+function get_better_excerpt( $post, $count = 150 ) {
+
+	if ( ! is_object( $post ) ) {
+		$post = get_post( $post );
+	}
+
+	$excerpt = '';
+	if ( is_object( $post ) ) {
+
+		$excerpt .= ( ! empty( $post->post_excerpt ) ) ? $post->post_excerpt : $post->post_content;
+
+		if ( $count < strlen( $excerpt ) ) { 
+			$excerpt  = substr( $excerpt, 0, $count ); 
+			$excerpt .= ' ...';
 		}
-
-		$excerpt = '';
-		if ( is_object( $post ) ) {
-
-			$excerpt .= ( ! empty( $post->post_excerpt ) ) ? $post->post_excerpt : $post->post_content;
-
-			if ( $count < strlen( $excerpt ) ) { 
-				$excerpt  = substr( $excerpt, 0, $count ); 
-				$excerpt .= ' ...';
-			}
-
-		}
-		
-		return $excerpt; 
 
 	}
+	
+	return $excerpt; 
+
+}
