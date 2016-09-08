f1e3dc05e18cabfa5c7302d19c922a4e2c708ec1
stormrockwell <hello@storms.email>
2016-09-08 09:44:45 -0400

    fixed function documentation

---
+++ get_better_excerpt.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Get Better Excerpt
+ * @param  mixed   $post  int or post object
+ * @param  integer $count max length of excerpt
+ * @return string         the excerpt
+ */
+function get_better_excerpt( $post, $count = 150 ) {
+
+	if ( ! is_object( $post ) ) {
+		$post = get_post( $post );
+	}
+
+	$excerpt = '';
+	if ( is_object( $post ) ) {
+		$excerpt .= ( ! empty( $post->post_excerpt ) ) ? $post->post_excerpt : $post->post_content;
+		if ( $count < strlen( $excerpt ) ) { 
+			$excerpt  = substr( $excerpt, 0, $count ); 
+			$excerpt .= ' ...';
+		}
+	}
+	
+	return $excerpt; 
+}
--- readme.md
+++ readme.md
@@ -2,6 +2,7 @@
 Simple function for better results than get_the_excerpt
 
 > get_better_excerpt( $post, $count = 150 )
+
 ## Parameters
 **$post**(int or object) - Either the post ID or the post object. Use the post object if available
 **$count**(int) - Length of the excerpt
